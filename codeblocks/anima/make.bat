881c45a2ef73258490a60fcd887375e0b5e82d8f
Nikita Dubrovsky <nekitgam@gmail.com>
2017-12-18 01:36:53 +0500

    Add Windows non-codeblocks compile

---
+++ codeblocks/anima/make.bat
@@ -0,0 +1,5 @@
+del anima.exe
+del obj\main.o
+mingw32-g++.exe -Wall -O2 -IC:\Users\Любовь\plastic_laboratory\LIBS\SDL-1.2.15\include -c main.cpp -o obj\main.o
+mingw32-g++.exe -LC:\Users\Любовь\plastic_laboratory\LIBS\SDL-1.2.15\lib -o anima.exe obj\main.o  -s  -lmingw32 -lSDLmain -lSDL.dll -luser32 -lgdi32 -lwinmm -ldxguid
+anima.exe
